6244d77ed19662b599fd865f3dd34348a604082b
Carlos Eduardo Lobo Arruda <kadu_lobo@hotmail.com>
2025-09-10 19:16:32 -0300

    ajuste teste

--- src/main/java/test/CofreControllerTest.java
+++ src/main/java/test/CofreControllerTest.java
@@ -1,8 +1,11 @@
 package test;
 
 import controller.CofreController;
+import controller.UsuarioController;
 import dao.impl.CofreDaoNativeImpl;
+import dao.impl.UsuarioDaoNativeImpl;
 import model.Cofre;
+import model.Usuario;
 
 import java.util.List;
 
@@ -10,9 +13,20 @@ public class CofreControllerTest {
     public static void main(String[] args) {
         CofreDaoNativeImpl dao = new CofreDaoNativeImpl();
         CofreController controller = new CofreController(dao);
+        
+        UsuarioDaoNativeImpl daoUsu = new UsuarioDaoNativeImpl();
+        UsuarioController controllerUsu = new UsuarioController(daoUsu);
 
+        // Recuperar usuário persistido e atualizar
+        List<Usuario> usuarios = controllerUsu.listar();
+        Usuario buscadoUsu = usuarios.get(0);
+        
         Cofre cofre = new Cofre();
         cofre.setLogin("login");
+        cofre.setIdUsuario(buscadoUsu.getIdUsuario());
+        cofre.setPlataforma("Plataforma x");
+        cofre.setSenha("123");
+        cofre.setTipo(1);
         controller.criar(cofre);
 
         List<Cofre> list = controller.listar();
